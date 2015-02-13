3c607226fd87c6b6b07dfbe95c0fa58bfae3370a
LingfeiXu <lxq35@hotmail.com>
2015-02-12 18:50:43 -0600

    modified

--- Repeated_DNA_Sequences.java
+++ Repeated_DNA_Sequences.java
@@ -22,3 +22,31 @@ public class Solution {
         return new ArrayList<String>(result);
     }
 }
+
+//public class Solution {
+//     public List<String> findRepeatedDnaSequences(String s) {
+//         List<String> result = new ArrayList<String>();
+//         Map<Character,Integer> map = new HashMap<Character,Integer>();
+//         map.put('A',0);
+//         map.put('C',1);
+//         map.put('G',2);
+//         map.put('T',3);
+//         if(s==null||s.length()<10)
+//             return new ArrayList<String>();
+//         HashSet<Integer> set = new HashSet<Integer>();
+//         HashSet<Integer> solution = new HashSet<Integer>();
+//         for(int i=0; i<=s.length()-10;i++) {
+//             int value = 0;
+//             for(int j=i; j<i+10; j++) {
+//                 value = (value << 2) | map.get(s.charAt(j));
+//             }
+//             if(set.contains(value)&&(!solution.contains(value))) {
+//                 result.add(s.substring(i,i+10));
+//                 solution.add(value);                
+//             }
+//             else
+//                 set.add(value);
+//         }
+//         return result;
+//     }
+// }
