bdc6fb2160bc15dbb43e6b113578d19eb4a276a5
LingfeiXu <lxq35@hotmail.com>
2015-03-08 22:21:10 -0500

    3/8

---
+++ binaryTreeLevelOrderTraversalII.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root == null)
+            return res;
+        List<Integer> pre = new ArrayList<Integer>();
+        pre.add(root.val);
+        res.add(pre);
+        List<TreeNode> level = new ArrayList<TreeNode>();
+        level.add(root);
+        int i = 0;
+        while(level.size() != 0) {
+            List nLevel = new ArrayList<TreeNode>();
+            List<Integer> cur = new ArrayList<Integer>();
+            i = 0;
+            while(i < level.size()) {
+                TreeNode cl = level.get(i);
+                if(cl.left!=null) {
+                    cur.add(cl.left.val);
+                    nLevel.add(cl.left);
+                }
+                if(cl.right!=null) {
+                    cur.add(cl.right.val);
+                    nLevel.add(cl.right);
+                }
+                i++;
+            }
+            if(cur.size()>0)
+                res.add(cur);
+            level = new ArrayList<TreeNode>(nLevel);
+            
+        }
+        List<List<Integer>> result = new ArrayList<List<Integer>>();
+        int j = res.size()-1;
+        while(j>=0) {
+            result.add(res.get(j));
+            j--;
+        }
+        return result;
+    }
+}
