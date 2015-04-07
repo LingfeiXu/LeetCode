a015e6554697e741a57ef9173c04ab9d54861630
LingfeiXu <lxq35@hotmail.com>
2015-04-07 15:31:00 -0500

    update

---
+++ binarySearchTreeIterator.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class BSTIterator {
+    Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        stack = new Stack<TreeNode>();
+        while(root!=null) {
+            stack.push(root);
+            root = root.left;
+        }
+    }
+
+    /** @return whether we have a next smallest number */
+    public boolean hasNext() {
+        if(!stack.empty())
+            return true;
+        else
+            return false;
+    }
+
+    /** @return the next smallest number */
+    public int next() {
+        int res = 0;
+        if(!stack.empty()) {
+            TreeNode current = stack.pop();
+            res = current.val;
+            if(current.right!=null) {
+                current = current.right;
+                while(current!=null) {
+                    stack.push(current);
+                    current = current.left;
+                }
+            }
+        }
+        return res;
+    }
+}
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = new BSTIterator(root);
+ * while (i.hasNext()) v[f()] = i.next();
+ */
