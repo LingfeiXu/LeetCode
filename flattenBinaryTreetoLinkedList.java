0cc12b563f32fd1e45dfcfac254d030ed0832411
LingfeiXu <lxq35@hotmail.com>
2015-04-09 10:36:14 -0500

    update

---
+++ flattenBinaryTreetoLinkedList.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private TreeNode lastVisit = null;
+    public void flatten(TreeNode root) {
+		if (root == null) {
+			return;
+		}
+		TreeNode savedRight = root.right;
+		if (lastVisit != null) {
+			lastVisit.left = null;
+			lastVisit.right = root;
+		}
+		lastVisit = root;
+		flatten(root.left);
+		flatten(savedRight);
+	}
+}
