63b4e0b7179261a58eca0233f4ee11f893d63723
LingfeiXu <lxq35@hotmail.com>
2015-04-10 11:33:28 -0500

    update

--- reverseWordsinaString.java
+++ reverseWordsinaString.java
@@ -1,3 +1,4 @@
+/*
 public class Solution {
     public String reverseWords(String s) {
         String res = "";
@@ -15,5 +16,34 @@ public class Solution {
             }
         }
         return res;
+    }
+}
+*/
+public class Solution {
+    public static String reverseWords(String s) {
+        if(s.isEmpty() || s.length() == 0)   return s;
+         
+        StringBuffer res = new StringBuffer();
+         
+        int t, h;
+        for(int i = s.length() - 1; i >= 0; i--) {
+            while(i >= 0 && s.charAt(i) == ' ') i--;
+             
+            // set tail pointer
+            if(i < 0) break;
+            t = i;
+            h = t;
+             
+            // set head pointer
+            while(i >= 0 && s.charAt(i) != ' ') { h = i; i--; }
+             
+            // append this word (append a space if find more than two words)
+            if(h <= t && res.length() > 0) res.append(' ');
+            for(int j = h; j <= t; j++) {
+                res.append(s.charAt(j));
+            }
+        }
+         
+        return res.toString();
     }
 }
