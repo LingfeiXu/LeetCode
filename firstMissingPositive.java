2cf943b938b64944e905acd655f27a71050b87b9
LingfeiXu <lxq35@hotmail.com>
2015-04-28 20:34:59 -0500

    update

--- firstMissingPositive.java
+++ firstMissingPositive.java
@@ -1,23 +1,24 @@
 public class Solution {
     public int firstMissingPositive(int[] nums) {
-        if(A.length==0||A==null)
+        if(nums.length==0||nums==null)
               return 1;
-            for(int i = 0;i<A.length;i++)
-            {
-              while(A[i]!=i+1)
-              {
-                if(A[i]>=A.length||A[i]<=0||A[i]==A[A[i]-1])
-                  break;
-                int temp = A[i];
-                A[i] = A[temp-1];
-                A[temp-1] = temp;
-              }
-            }
-            
-            for(int i = 0;i<A.length;i++)
-            {
-              if(A[i]!=i+1)
-                return i+1;
-            }
-            return A.length+1;
+        for(int i = 0;i<nums.length;i++)
+        {
+          while(nums[i]!=i+1)
+          {
+            if(nums[i]>=nums.length||nums[i]<=0||nums[i]==nums[nums[i]-1])
+              break;
+            int temp = nums[i];
+            nums[i] = nums[temp-1];
+            nums[temp-1] = temp;
+          }
+        }
+        
+        for(int i = 0;i<nums.length;i++)
+        {
+          if(nums[i]!=i+1)
+            return i+1;
+        }
+        return nums.length+1;
+    }
 }
